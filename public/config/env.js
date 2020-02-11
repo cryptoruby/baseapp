15d5581358260646e049bc3d93c70da91482a117
ruby261 <56286981+ruby261@users.noreply.github.com>
2020-02-11 22:53:41 +0545

    Update env.js

--- public/config/env.js
+++ public/config/env.js
@@ -3,7 +3,7 @@ window.env = {
         authUrl: 'http://localhost:9002/api/v2/barong',
         tradeUrl: 'http://localhost:9002/api/v2/peatio',
         applogicUrl: 'http://localhost:9002/api/v2/applogic',
-        rangerUrl: '',
+        rangerUrl: 'http://localhost:9002/api/v2/ranger',
     },
     minutesUntilAutoLogout: '60',
     withCredentials: false,
